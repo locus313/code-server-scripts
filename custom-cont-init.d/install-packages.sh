d42d76b8e3dcf412514b023d3fab59930511d396
Patrick Lewis <pat@lo5t.com>
2022-08-14 03:29:31 -0700

    initial version of install-packages.sh

---
+++ custom-cont-init.d/install-packages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "**** updating the list of packages ****"
+apt-get update
+
+echo "**** installing pre-requisite packages ****"
+apt-get install -y wget apt-transport-https software-properties-common
+
+echo "**** downloading the microsoft repository gpg keys ****"
+wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
+
+echo "**** installing the microsoft repository gpg keys ****"
+dpkg -i packages-microsoft-prod.deb
+
+echo "**** updating the list of packages after adding repos ****"
+apt-get update
+
+echo "**** installing powershell ****"
+apt-get -y install powershell
